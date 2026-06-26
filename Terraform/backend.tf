690ec52405b12f351db311e467f745e39d3ea695
nada essam <nadaessam99e@gmail.com>
2026-06-26 15:45:55 +0300

    Update S3 bucket name for Terraform backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "1.15.2"
 
   backend "s3" {
-    bucket         = "kubevision-tf-state-439328746854-us-east-1-an"               
+    bucket         = "kubevision-tf-state-114223852322-us-east-1-an"               
     key            = "kubevision/terraform.tfstate"   
     region         = "us-east-1"                      
     encrypt        = true                             
